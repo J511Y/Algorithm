d84023f8dc65b8e67cc7ee8cf75ca693218077a2
J511Y <47891005+J511Y@users.noreply.github.com>
2020-02-19 23:39:59 +0900

    [세그먼트 트리][백준_2357_최솟값과 최댓값_맞왜틀]

---
+++ skill_check/workspace/src/baekjoon_basic/baekjoon_2357.java
@@ -0,0 +1,92 @@
+package baekjoon_basic;
+import java.util.*;
+import java.io.*;
+public class baekjoon_2357 {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static Node[] seg;
+	static int n, m, level, len, a, b, start;
+	public static void main(String[] ar) throws Exception{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		level = level(n);
+		len = 1 << level;
+		seg = new Node[len + 1];
+		for(int i = 0; i < len + 1; i++) seg[i] = new Node();
+		start = 1 << (level - 1);
+		for(int i = start; i < seg.length; i++){
+			if(i >= start + n) {
+				seg[i].max = -1;
+				seg[i].min = 1000000001;
+			}else {
+				seg[i] = new Node(Integer.parseInt(br.readLine()));				
+			}
+		}
+		init();
+		for(int i = 1; i < seg.length; i++) System.out.println(seg[i]);
+		for(int i = 0; i < m; i++){
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			Node node = find(a, b, 1, 1, len / 2);
+			bw.write(node.min + " " + node.max + "\n");
+		}
+		bw.flush();
+	}
+	
+	static int level(int a){
+		if(a == 0) return 1;
+		return 1 + level(a / 2);
+	}
+	
+	static void init(){
+		int i = ((len + 1) / 4);
+		while(i > 0){
+			for(int x = i; x < i * 2; x++){
+				seg[x].min = Math.min(seg[x * 2].min, seg[x * 2 + 1].min);
+				seg[x].max = Math.max(seg[x * 2].max, seg[x * 2 + 1].max);
+			}
+			i /= 2;
+		}
+	}
+	
+	static Node find(int a, int b, int idx, int s, int e){
+		int half = (s + e) / 2;
+		Node node = new Node();
+		if(a <= s && e <= b) return seg[idx];
+		else if(a <= half){
+			node = find(a, b, idx * 2, s, half);
+			if(b > half) {
+				Node other = find(a, b, idx * 2 + 1, half + 1, e);
+				node.min = Math.min(node.min, other.min);
+				node.max = Math.max(node.max, other.max);
+			}
+		}else if(half <= a){
+			node = find(a, b, idx * 2 + 1, half + 1, e);
+			if(b < half) {
+				Node other = find(a, b, idx * 2, s, half);
+				node.min = Math.min(node.min, other.min);
+				node.max = Math.max(node.max, other.max);
+			}
+		}
+		return node;
+	}
+}
+
+class Node{
+	int min;
+	int max;
+	Node(){}
+	Node(int val){
+		this.min = val;
+		this.max = val;
+	}
+	@Override
+	public String toString() {
+		return "Node [min=" + min + ", max=" + max + "]";
+	}
+	
+}
