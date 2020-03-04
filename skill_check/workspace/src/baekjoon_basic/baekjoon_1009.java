072af479ddbe049a90bd83d8465bf54d148615d3
J511Y <47891005+J511Y@users.noreply.github.com>
2020-03-05 01:22:01 +0900

    [수학][백준_1009_분산처리]

---
+++ skill_check/workspace/src/baekjoon_basic/baekjoon_1009.java
@@ -0,0 +1,17 @@
+package baekjoon_basic;
+// https://www.acmicpc.net/problem/1009
+// 분산처리
+
+// A를 B번 제곱해서 나오는 1의 자리를 묻는 문제
+import java.util.*;
+public class baekjoon_1009 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt(), b = sc.nextInt(), c = a % 10;
+			for(int j = 1; j < b; j++) c = (c * a) % 10;
+			System.out.println(c == 0 ? 10 : c);
+		}
+	}
+}
