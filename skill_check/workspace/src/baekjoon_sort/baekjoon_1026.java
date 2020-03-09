c17eb6f9cea58423cd2d71e0b668c550a7746703
J511Y <47891005+J511Y@users.noreply.github.com>
2020-03-09 22:25:07 +0900

    [정렬][백준_1026_보물]

---
+++ skill_check/workspace/src/baekjoon_sort/baekjoon_1026.java
@@ -0,0 +1,17 @@
+package baekjoon_sort;
+// https://www.acmicpc.net/problem/1026
+// º¸¹°
+import java.util.*;
+public class baekjoon_1026 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), sum = 0;
+		int[] a = new int[n], b = new int[n];
+		for(int i = 0; i < n; i++) a[i] = sc.nextInt();
+		for(int i = 0; i < n; i++) b[i] = sc.nextInt();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		for(int i = 0; i< n; i++) sum += a[i] * b[n - i - 1];
+		System.out.println(sum);
+	}
+}
