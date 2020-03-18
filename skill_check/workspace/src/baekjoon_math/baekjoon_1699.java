c0f6739414d7704bb3de80924dd420e1ff94e03b
J511Y <47891005+J511Y@users.noreply.github.com>
2020-03-18 23:30:44 +0900

    [수학][백준_1699_제곱수의 합][시간 비교]

--- skill_check/workspace/src/baekjoon_math/baekjoon_1699.java
+++ skill_check/workspace/src/baekjoon_math/baekjoon_1699.java
@@ -7,6 +7,7 @@ package baekjoon_math;
  * 제곱근에 제일 가까운 수로 계속 빼보면 되지 않을까 생각
  */
 public class baekjoon_1699 {
+	// recursion
 	static int min = 999;
 	static void func(int n, int cnt) {
 		if(cnt >= min) return;
@@ -16,7 +17,30 @@ public class baekjoon_1699 {
 	}
 	public static void main(String[] args) {
 		int n = new java.util.Scanner(System.in).nextInt();
+		System.out.println("===========bottom-up============");
+		double start = System.currentTimeMillis();
+		func(n);
+		double end = System.currentTimeMillis();
+		System.out.println("time : " + (end - start) / 1000 + "s");
+		
+		
+		System.out.println("===========recursion============");
+		start = System.currentTimeMillis();
 		func(n, 0);
+		end = System.currentTimeMillis();
 		System.out.println(min);
+		System.out.println("time : " + (end - start) / 1000 + "s");
+	}
+	
+	// bottom-up
+	static void func(int n) {
+		int[] dp = new int[n + 1];
+		for(int i = 1; i <= n; i++) {
+			dp[i] = i;
+			for(int j = 1; j * j <= i; j++) {
+				dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+			}
+		}
+		System.out.println(dp[n]);
 	}
 }
