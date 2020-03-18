4b09cc397b75412ddfcba18b2e8c11015e4b8055
J511Y <47891005+J511Y@users.noreply.github.com>
2020-03-18 22:49:30 +0900

    [DFS][백준_2583_영역 구하기][왜 n,m 거꾸로 주냐 ㅡㅡ]

--- skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2583.java
+++ skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2583.java
@@ -6,27 +6,27 @@ public class baekjoon_2583 {
 	static int cnt = 0, n, m;
 	static void dfs(boolean[][] map, int i, int j) {
 		map[i][j] = true; cnt++;
-		for(int x=0; x<4; x++) {
+		for(int x = 0; x < 4; x++) {
 			int ii = i + wh[0][x], jj = j + wh[1][x];
-			if(0 <= ii && ii < n && 0 <= jj && jj < m) dfs(map, ii, jj);
+			if(0 <= ii && ii < m && 0 <= jj && jj < n && !map[ii][jj]) dfs(map, ii, jj);
 		}
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt(); m = sc.nextInt(); int k = sc.nextInt();
-		boolean[][] map = new boolean[n][m];
+		m = sc.nextInt(); n = sc.nextInt(); int k = sc.nextInt();
+		boolean[][] map = new boolean[m][n];
 		for(int i=0; i<k; i++) {
 			int a = sc.nextInt(), b = sc.nextInt();
 			int c = sc.nextInt(), d = sc.nextInt();
 			for(; a < c; a++) {
 				for(int bb = b; bb < d; bb++) {
-					map[a][bb] = true;
+					map[bb][a] = true;
 				}
 			}
 		}
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
 				if(!map[i][j]) {
 					dfs(map, i, j);
 					list.add(cnt); cnt = 0;
@@ -34,6 +34,7 @@ public class baekjoon_2583 {
 			}
 		}
 		list.sort(null);
-		for(int i : list) System.out.println(i);
+		System.out.println(list.size());
+		for(int i : list) System.out.print(i+" ");
 	}
 }
