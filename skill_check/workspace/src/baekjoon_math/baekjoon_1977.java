436cac0b8e12b0961c484488dd55c58b11823800
CafeAlle <CafeAlle@PC049>
2020-03-27 20:28:45 +0900

    [수학][백준_1977_완전제곱수]

---
+++ skill_check/workspace/src/baekjoon_math/baekjoon_1977.java
@@ -0,0 +1,20 @@
+package baekjoon_math;
+// https://www.acmicpc.net/problem/1977
+// 완전제곱수
+import java.util.*;
+public class baekjoon_1977 {
+	static int[] pow = new int[101];
+	static { for(int i = 1; i <= 100; i++) pow[i] = i * i;}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt(), n = sc.nextInt(), sum = 0, min = 10001;
+		for(int i = 1; i < 101; i++) {
+			int val = pow[i];
+			if(m <= val && val <= n) {
+				min = Math.min(val, min);
+				sum += val;
+			}
+		}
+		System.out.println(sum == 0 ? "-1": (sum+"\n"+min));
+	}
+}
