53b5471391e29ad86a129236514384bdc32de2a3
CafeAlle <CafeAlle@PC049>
2020-03-27 20:22:06 +0900

    [수학][백준_2777_숫자 놀이]

---
+++ skill_check/workspace/src/baekjoon_math/baekjoon_2777.java
@@ -0,0 +1,31 @@
+package baekjoon_math;
+// https://www.acmicpc.net/problem/2777
+// ¼ıÀÚ ³îÀÌ
+import java.util.*;
+class baekjoon_2777 {
+	static int n;
+	static int dev(int x) {
+		int cnt = 0;
+		while(n % x == 0) {
+			n /= x;
+			cnt++;
+		}
+		return cnt;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for(int i = 0; i < tc; i++) {
+			n = sc.nextInt();
+			if(n < 10) {
+				System.out.println(1);
+				continue;
+			}
+			int two = dev(2) + 2;
+			int thr = dev(3) + 1;
+			int oth = dev(5) + dev(7);
+			int six = (two % 3 == 0 && thr % 2 == 0) ? 1 : 0;
+			System.out.println(n == 1 ? ((two / 3) + (thr / 2) + oth - six) : -1);
+		}
+	}
+}
