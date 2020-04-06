04687f264bc4d5bd7740b9dac67d91a543673514
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-06 23:54:52 +0900

    [이분탐색][프로그래머스_43236_징검다리]

--- skill_check/workspace/src/programmers/prgmers_43236.java
+++ skill_check/workspace/src/programmers/prgmers_43236.java
@@ -4,11 +4,29 @@ package programmers;
 import java.util.*;
 public class prgmers_43236 {
 	public static int solution(int distance, int[] rocks, int n) {
-		int len = rocks.length, answer = 0;
-        if(n == len) return distance;
-        Arrays.sort(rocks);
-        
-        return answer;
+        int len = rocks.length, left = 1, right = 1 << 30, max = 0;
+        int[] nrocks = new int[len + 2];
+        nrocks[len] = distance;
+        for(int i = 0; i < len; i++) nrocks[i] = rocks[i];
+        Arrays.sort(nrocks);
+        while(left <= right){
+            int[] temp = Arrays.copyOf(nrocks, len + 2);
+            int mid = (left / 2) + (right / 2), cnt = 0;
+            for(int i = 0; i < len; i++) {
+                if((temp[i+1] - temp[i]) < mid){
+                    cnt++;
+                    temp[i+1] = temp[i];
+                }
+            }
+            if(cnt > n){
+                right = mid - 1;
+            }else{
+                max = Math.max(max, mid);
+                left = mid + 1;
+            }
+            
+        }
+        return max;
     }
 	public static void main(String[] args) {
 		System.out.println(solution(25, new int[] {2, 14, 11, 21, 17}, 2));
