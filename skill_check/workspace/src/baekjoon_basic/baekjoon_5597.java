53481a2595dbcd3b4b5bbed0cb0809604206446a
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-06 22:57:01 +0900

    [기본][백준_5597_과제 안내신 분...?]

---
+++ skill_check/workspace/src/baekjoon_basic/baekjoon_5597.java
@@ -0,0 +1,14 @@
+package baekjoon_basic;
+// https://www.acmicpc.net/problem/5597
+// 과제 안내신 분...?
+import java.util.*;
+public class baekjoon_5597 {
+	public static void main(String[] ar){
+		Scanner sc = new Scanner(System.in);
+		int[] students = new int[31];
+		for(int i = 0; i < 28; i++) students[sc.nextInt()] = 1;
+		for(int i = 1; i < 31; i++) {
+			if(students[i] == 0) System.out.println(i);
+		}
+	}
+}
