c8280365446d1b06353ee97f9eaf8b4c5b8a2488
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-06 22:40:53 +0900

    [수학][프로그래머스_12936_줄 서는 방법]

---
+++ skill_check/workspace/src/programmers/prgmers_12936.java
@@ -0,0 +1,35 @@
+package programmers;
+// https://programmers.co.kr/learn/courses/30/lessons/12936?language=java
+// 줄 서는 방법
+import java.util.*;
+public class prgmers_12936 {
+	static long[] fact = new long[21];
+    static{
+        long f = 1;
+        for(int i = 1; i < 21; i++){
+            f *= i;
+            fact[i] = f;
+        }
+    }
+    public int[] solution(int n, long k) {
+        k--;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) list.add(i);
+        int[] answer = new int[n];
+        int idx = 0;
+        for(long i = n - 1; i > 0; i--){
+            long mok = k / fact[(int)i];
+            answer[idx++] = list.get((int)mok);
+            list.remove((int)mok);
+            k %= fact[(int)i];
+        }
+        answer[idx] = list.get(0);
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
