2ea4f56fda909341ef9620cf59f97d5d3a9b6748
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-10 21:48:38 +0900

    [문자열][백준_1254_팰린드롬 만들기]

---
+++ skill_check/workspace/src/baekjoon_string/baekjoon_1254.java
@@ -0,0 +1,22 @@
+package baekjoon_string;
+// https://www.acmicpc.net/problem/1254
+// 팰린드롬 만들기
+public class baekjoon_1254 {
+	static boolean isPalin(String s) {
+		int len = s.length();
+		for(int i = 0; i < len / 2; i++) {
+			if(s.charAt(i) != s.charAt(len - i - 1)) return false;
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		String s = new java.util.Scanner(System.in).next();
+		int len = s.length();
+		for(int i = 0; i < len; i++) {
+			if(isPalin(s.substring(i))) {
+				System.out.println(len + i);
+				return;
+			}
+		}
+	}
+}
