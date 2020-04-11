909394dc265cbb3c1eee87868295612a1c6de6dc
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-11 22:49:48 +0900

    [분할정복][백준_1629_곱셈]

---
+++ skill_check/workspace/src/baekjoon_DivideAndConquer/baekjoon_1629.java
@@ -0,0 +1,18 @@
+package baekjoon_DivideAndConquer;
+// https://www.acmicpc.net/problem/1629
+// °ö¼À
+import java.util.*;
+public class baekjoon_1629 {
+	static long divide(long a, long b, long c) {
+		if(b == 1) return a;
+		long tmp = divide(a, b / 2, c);
+		if(b % 2 == 0) return (tmp * tmp) % c;
+		else return (((tmp * tmp) % c) * a) % c;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+		System.out.println(divide(a % c, b, c));
+		System.out.println(Integer.MAX_VALUE);
+	}
+}
