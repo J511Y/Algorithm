b0dbcdbf8eea2a32e044696925ed90e406cd1dd5
유준혁 <47891005+J511Y@users.noreply.github.com>
2020-04-13 20:26:10 +0900

    [수학][백준_1748_수 이어 쓰기1]

---
+++ skill_check/workspace/src/baekjoon_math/baekjoon_1748.java
@@ -0,0 +1,8 @@
+class baekjoon_1748{
+	public static void main(String[] ar){
+		int n = new java.util.Scanner(System.in).nextInt();
+        long cnt = 0;
+		for(int i = 1; i <= n; i *= 10) cnt += n - i + 1;
+		System.out.println(cnt);
+	}
+}
