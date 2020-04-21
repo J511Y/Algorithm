e11a4e8d5d6d1d748ae8e12d72d7c54b2dd5162e
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-21 23:16:17 +0900

    [브루트포스][백준_10448_유레카 이론]

---
+++ skill_check/workspace/src/baekjoon_brute_force/baekjoon_10448.java
@@ -0,0 +1,23 @@
+package baekjoon_brute_force;
+// https://www.acmicpc.net/problem/10448
+// 유레카 이론
+import java.util.*;
+public class baekjoon_10448 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); 
+		int[] tri = new int[45];
+		for(int i = 1; i < 45; i++) tri[i] = tri[i-1] + i;
+		for(int i = 0; i < n; i++) {
+			int x = sc.nextInt(), rst = 0;
+			for(int a = 1; a < 45 && rst == 0; a++) {
+				for(int b = 1; b < 45 && rst == 0; b++) {
+					for(int c = 1; c < 45 && rst == 0; c++) {
+						if(tri[a] + tri[b] + tri[c] == x) rst = 1;
+					}
+				}
+			}
+			System.out.println(rst);
+		}
+	}
+}
