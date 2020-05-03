0791093f4a3441744043eed5b985d1f9105f53c7
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-03 20:04:31 +0900

    [메서드연습][백준_1357_뒤집힌 덧셈]

---
+++ skill_check/workspace/src/baekjoon_basic/baekjoon_1357.java
@@ -0,0 +1,14 @@
+package baekjoon_basic;
+// https://www.acmicpc.net/problem/1357
+// µÚÁýÈù µ¡¼À
+import java.util.*;
+public class baekjoon_1357 {
+	static int Rev(String a) {
+		return Integer.parseInt(new StringBuffer(a).reverse().toString());
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next(), b = sc.next();
+		System.out.println(Rev(Rev(a) + Rev(b) + ""));
+	}
+}
