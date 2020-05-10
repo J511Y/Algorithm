96fc5a48e4d0f5a46a1592e303ae4b9347e0cb29
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-10 21:04:44 +0900

    [그래프탐색][백준_1058_친구]

---
+++ skill_check/workspace/src/baekjoon_graph/baekjoon_1058.java
@@ -0,0 +1,32 @@
+package baekjoon_graph;
+// https://www.acmicpc.net/problem/1058
+// Ä£±¸
+import java.util.*;
+public class baekjoon_1058 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), max = 0;
+		int[][] map = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			byte[] b = sc.next().getBytes();
+			for(int j = 0; j < n; j++) map[i][j] = b[j] == 'Y' ? 1 : 0;
+		}
+		for(int i = 0; i < n; i++) {
+			int cnt = 0;
+			for(int j = 0; j < n; j++) {
+				if(i == j) continue;
+				if(map[i][j] == 1) cnt++;
+				else {
+					for(int k = 0; k < n; k++) {
+						if(map[i][k] == 1 && map[k][j] == 1) {
+							cnt++;
+							break;
+						}
+					}
+				}
+			}
+			max = Math.max(max, cnt);
+		}
+		System.out.print(max);
+	}
+}
