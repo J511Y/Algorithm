a9fdba08b149391ea05cdb27f72173f4e55abb72
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-12 20:28:09 +0900

    [수학][백준_18291_비요뜨의 징검다리 건너기]

---
+++ skill_check/workspace/src/baekjoon_math/baekjoon_18291.java
@@ -0,0 +1,19 @@
+package baekjoon_math;
+// https://www.acmicpc.net/problem/18291
+// 징검다리 건너기
+import java.util.*;
+class baekjoon_18291{
+    public static void main(String[] ar){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), mod = 1000000007;
+        for(int tc = 0; tc < n; tc++){
+            long x = sc.nextLong() - 2, val = 1, two = 2;
+            while(x > 0){
+                if(x % 2 == 1) val = (val * two) % mod;
+        		x /= 2;
+        		two = (two * two) % mod;
+            }
+            System.out.println(val);
+        }
+    }
+}
