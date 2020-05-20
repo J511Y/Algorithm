ce1b6bf55e2713c78a8cd19c90f6bffbe00f7455
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-20 23:40:23 +0900

    [기본][백준_18108_1998년생인 내가 태국에서는 2541년생?!]

---
+++ skill_check/workspace/src/baekjoon_basic/baekjoon_18108.java
@@ -0,0 +1,8 @@
+package baekjoon_basic;
+// https://www.acmicpc.net/problem/18108
+// 1998년생인 내가 태국에서는 2541년생?!
+class baekjoon_18108 {
+    public static void main(String args[]){ 
+        System.out.print(new java.util.Scanner(System.in).nextInt() - 543);
+    }
+}
