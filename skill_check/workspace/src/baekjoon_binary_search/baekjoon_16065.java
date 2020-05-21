f8806e0e4061c77f455c45fb86aec6ba5c6668f4
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-21 20:55:43 +0900

    [애드혹][백준_16065_Down the Pyramid]

--- skill_check/workspace/src/baekjoon_binary_search/baekjoon_16065.java
+++ skill_check/workspace/src/baekjoon_binary_search/baekjoon_16065.java
@@ -9,27 +9,15 @@ public class baekjoon_16065 {
 	}
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Int(br.readLine());
-		int[] arr = new int[n];
+		int n = Int(br.readLine()), min = 1 << 30;
+		int[] arr = new int[n+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) arr[i] = Int(st.nextToken());
-		int left = 0, right = arr[0];
-		while(left <= right) {
-			int mid = (left + right) / 2, tmp = mid;
-			boolean success = true;
-			for(int i = 0; i < n; i++) {
-				tmp = arr[i] - tmp;
-				if(tmp < 0) {
-					success = false;
-					break;
-				}
-			}
-			if(success) right = mid - 1;
-			else left = mid + 1;
-		}
-		for(int i = 0; i < n; i++) arr[i] = left = arr[i] - left;
-		int min = 1 << 30;
-		for(int i = 0; i < n; i += 2) min = Math.min(min, arr[i] + 1);
-		System.out.print(min < 0 ? 0 : min);
+		for(int i = n - 1; i >= 0; i--) arr[i] -= arr[i+1];
+		for(int i = 0; i <= n; i += 2) min = Math.min(min, arr[i]);
+		for(int i = 0; i <= n; i++) arr[i] += min * (i % 2 == 0 ? -1 : 1);
+		min = 1 << 30;
+		for(int i = 1; i <= n; i += 2) min = Math.min(min, arr[i] + 1);
+		System.out.println(min < 0 ? 0 : min);
 	}
 }
