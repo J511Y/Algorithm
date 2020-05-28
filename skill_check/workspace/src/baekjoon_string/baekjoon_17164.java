d84913eefde1be50bbec20a7e9ec1dd84245ff0c
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-28 23:00:02 +0900

    [문자열][백준_17164_레인보우 비즈]

---
+++ skill_check/workspace/src/baekjoon_string/baekjoon_17164.java
@@ -0,0 +1,25 @@
+package baekjoon_string;
+// https://www.acmicpc.net/problem/17164
+// 레인보우 비즈
+import java.util.*;
+public class baekjoon_17164 {
+	public static void main(String[] args) {
+		Scanner sc = new java.util.Scanner(System.in);
+		sc.nextInt();
+		String[] input = sc.next().split("V");
+		int max = 1;
+		for(String s : input) {
+			byte[] arr = s.getBytes();
+			int cnt = 1;
+			for(int i = 1; i < arr.length; i++) {
+				if(arr[i] != arr[i-1]) cnt++;
+				else {
+					max = Math.max(cnt, max);
+					cnt = 1;
+				}
+			}
+			max = Math.max(cnt, max);
+		}
+		System.out.println(max);
+	}
+}
