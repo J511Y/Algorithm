131dc98f1cd84348ef32a34eab1696097a07e1a7
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-28 21:52:11 +0900

    [Map][백준_7785_회사에 있는 사람]

---
+++ skill_check/workspace/src/baekjoon_hashmap/baekjoon_7785.java
@@ -0,0 +1,16 @@
+package baekjoon_hashmap;
+// https://www.acmicpc.net/problem/7785
+// 회사에 있는 사람
+import java.util.*;
+public class baekjoon_7785 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Map<String, String> map = new HashMap<>();
+		for(int i = 0; i < n; i++) map.put(sc.next(), sc.next());
+		List<String> list = new ArrayList<>();
+		list.addAll(map.keySet());
+		list.sort(Collections.reverseOrder());
+		for(String s : list) if(map.get(s).equals("enter")) System.out.println(s);
+	}
+}
