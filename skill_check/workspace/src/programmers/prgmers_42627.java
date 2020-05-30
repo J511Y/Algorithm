5e7f861f97ea76fab0a0c87dcd071fbc483e99c5
J511Y <47891005+J511Y@users.noreply.github.com>
2020-05-31 04:22:45 +0900

    [우선순위큐][프로그래머스_42627_디스크 컨트롤러]

--- skill_check/workspace/src/programmers/prgmers_42627.java
+++ skill_check/workspace/src/programmers/prgmers_42627.java
@@ -3,32 +3,36 @@ package programmers;
 // 디스크 컨트롤러
 import java.util.*;
 public class prgmers_42627 {
-	public int solution(int[][] jobs) {
-        int answer = 0, time = 0;
-        Arrays.sort(jobs);
+	static public int solution(int[][] jobs) {
+		int answer = 0, time = 0;
+        Arrays.sort(jobs, (a, b)->{return (a[0] - b[0] == 0 ? a[1] - b[1] : a[0] - b[0]);});
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->{
             return a[1] - b[1];
         });
-        for(int i = 0; i < jobs.length || !pq.isEmpty(); i++){
-        	if(i < jobs.length) {
-        		int[] job = jobs[i];
-        		if(time <= job[0]) {
-        			pq.add(job);
-        			time += job[0];
-        			continue;
-        		}        		
-        	}
-            if(!pq.isEmpty()) {
-            	int[] poll = pq.poll();
-            	answer += poll[1] + time - poll[0];
-            }else {
-            	time = jobs[i][0];
+        for(int i = 0; i < jobs.length; i++){
+        	if(time >= jobs[i][0]) {
+        		pq.add(jobs[i]);
+        	}else{
+                if(!pq.isEmpty()) {
+                    int[] poll = pq.poll();
+                    time += poll[1];
+                    answer += time - poll[0];
+                    i--;
+                }else {
+                    time = jobs[i][0] + jobs[i][1];
+                    answer += time - jobs[i][0];
+                }
             }
+        }
+        while(!pq.isEmpty()){
+            int[] poll = pq.poll();
+            time += poll[1];
+            answer += time - poll[0];
         }
         return answer / jobs.length;
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {1, 1}, {2, 6}}));
 
 	}
 }
