d65f70bef36a58bb53b8edc59b0e82a28dd081bb
J511Y <47891005+J511Y@users.noreply.github.com>
2020-06-14 22:06:16 +0900

    [구현][백준_2232_지뢰]

---
+++ skill_check/workspace/src/baekjoon_simulation/baekjoon_2232.java
@@ -0,0 +1,17 @@
+package baekjoon_simulation;
+// https://www.acmicpc.net/problem/2232
+// Áö·Ú
+import java.util.*;
+public class baekjoon_2232 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), cnt = 0;
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			if((i == 0 || arr[i - 1] <= arr[i]) && (i == n - 1 || arr[i + 1] <= arr[i])) {
+				System.out.println(i + 1);
+			}
+		}
+	}
+}
