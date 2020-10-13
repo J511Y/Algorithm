a45a73b7dbec4bc50db07c7fc193f3ffcba49454
J511Y <47891005+J511Y@users.noreply.github.com>
2020-10-13 22:04:02 +0900

    백준_BFS_16953

---
+++ skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_16953.java
@@ -0,0 +1,23 @@
+package baekjoon_DFS_BFS;
+
+import java.util.*;
+public class baekjoon_16953 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt(), rst = -1;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {a, 1});
+		
+		while(!q.isEmpty()) {
+			int[] poll = q.poll();
+			int x = poll[0], d = poll[1];
+			if(x == b) {
+				rst = d;
+				break;
+			}
+			if(x * 2 <= b) q.add(new int[] {x * 2, d + 1});
+			if(x * 10 + 1 <= b) q.add(new int[] {x * 10 + 1, d + 1});
+		}
+		System.out.println(rst);
+	}
+}
