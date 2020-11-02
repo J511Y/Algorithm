e5110bac5077d1ff43e45bcc8d45fe5c8d507ea1
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-03 02:27:17 +0900

    ㅇㅅㅇ

--- skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2573.java
+++ skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2573.java
@@ -4,69 +4,62 @@ package baekjoon_DFS_BFS;
 import java.util.*;
 public class baekjoon_2573 {
 	static int[][] WH = {{1, 0, -1, 0}, {0, 1, 0, -1}};
-	static int[][] BFS(int[][] map, int x, int y){
-		int n = map.length, m = map[0].length;
-		map[x][y] = 0;
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x, y});
-		while(!q.isEmpty()) {
-			int[] poll = q.poll();
-			x = poll[0];
-			y = poll[1];
-			for(int i = 0; i < 4; i++) {
-				int X = x + WH[0][i];
-				int Y = y + WH[1][i];
-				if(0 <= X && X < n && 0 <= Y && Y < m && map[X][Y] > 0) {
-					q.add(new int[] {X, Y});
-					map[X][Y] = 0;
-				}
-			}
-		}
-		return map;
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(), m = sc.nextInt();
-		int[][] map = new int[n][m];
-		for(int i = 0; i < n * m; i++) map[i/m][i%m] = sc.nextInt();
-		for(int i = 0; i < 10; i++) {
-			int j = 0;
-			int[][] minus = new int[n][m];
-			for(; j < n * m && i > 0; j++) {
-				int x = j/m, y = j%m;
-				for(int k = 0; k < 4; k++) {
-					int X = x + WH[0][k];
-					int Y = y + WH[1][k];
-					if(0 <= X && X < n && 0 <= Y && Y < m && map[X][Y] < 1) {
-						minus[x][y]++;				
+	static int count(int[][] map){
+		int n = map.length, m = map[0].length, cnt = 0;
+		int[][] vst = new int[n][m];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(map[i][j] != 0 && vst[i][j] == 0) {
+					cnt++;
+					vst[i][j] = 1;
+					Queue<int[]> q = new LinkedList<>();
+					q.add(new int[] {i, j});
+					while(!q.isEmpty()) {
+						int[] poll = q.poll();
+						int a = poll[0], b = poll[1];
+						for(int k = 0; k < 4; k++) {
+							int x = a + WH[0][k];
+							int y = b + WH[1][k];
+							if(0 <= x && x < n && 0 <= y && y < m && map[x][y] > 0 && vst[x][y] == 0) {
+								q.add(new int[] {x, y});
+								vst[x][y] = 1;
+							}
+						}
 					}
 				}
 			}
-			for(j = 0; j < n * m; j++) map[j/m][j%m] -= minus[j/m][j%m];
-			System.out.println();
-			for(int[] arr : map) {
-				System.out.println(Arrays.toString(arr));
-			}
-			int[][] clone = new int[n][m];
-			for(j = 0; j < n * m; j++) clone[j/m][j%m] = map[j/m][j%m];
-			for(j = 0; j < n * m; j++) {
-				if(clone[j/m][j%m] > 0) {
-					clone = BFS(clone, j/m, j%m);
-					break;
-				}
-			}
-			System.out.println();
-			for(int[] arr : clone) {
-				System.out.println(Arrays.toString(arr));
-			}
-			System.out.println("================");
-			for(; j < n * m; j++) {
-				if(clone[j/m][j%m] > 0) {
-					System.out.println(i);
-					return;
-				}
+		}
+		return cnt;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), m = sc.nextInt(), year = 0;
+		int[][] map = new int[n][m];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				map[i][j] = sc.nextInt();
 			}
 		}
-		System.out.println(0);
+		while(count(map) == 1) {
+			year++;
+			int[][] tmp = new int[n][m];
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					int cnt = 0;
+					for(int k = 0; k < 4; k++) {
+						int a = i + WH[0][k];
+						int b = j + WH[1][k];
+						if(0 <= a && 0 <= b && a < n && b < m && map[a][b] == 0) cnt++;
+					}
+					tmp[i][j] = Math.max(0, map[i][j] - cnt);
+				}
+			}
+			
+			map = tmp;
+		}
+		int sum = 0;
+		for(int[] arr : map) for(int i : arr) sum += i;
+		if(sum == 0) year = 0;
+		System.out.println(year);
 	}
 }
