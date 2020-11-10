a01969bdd34c4832bb036b4eff88668174c52fe1
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-10 13:01:10 +0900

    commit

--- skill_check/workspace/src/Main.java
+++ skill_check/workspace/src/Main.java
@@ -3,11 +3,23 @@ import java.util.*;
 class Main {
 	public static void main(String[] ar) {
 		Main m = new Main();
-		func(1);
+		System.out.println((1 + Integer.MAX_VALUE) == Integer.MIN_VALUE);
 	}
     
-    static void func(int n){
-    	if(n == 200000) return;
-    	func(n + 1);
+    static int func(int n){
+    	int left = 1, right = Integer.MAX_VALUE;
+    	while(left <= right){
+    		int mid = (left + right) / 2;
+    		// a = 1부터 mid까지의 합
+    		// b = 1부터 mid + 1 까지의 합
+    		int a, b;
+    		
+    		// 나올 수 있는 조건은 3가지
+    		// n이 a 보다 작은 경우
+    		// n이 b 보다 같거나 큰 경우
+    		// 그 외
+    		
+    	}
+    	return left;
     }
 }
---
+++ skill_check/workspace/src/baekjoon_greedy/baekjoon_1339.java
@@ -0,0 +1,52 @@
+package baekjoon_greedy;
+
+import java.util.*;
+public class baekjoon_1339 {
+	static String[] arr;
+	static char[] carr = new char[10];
+	static boolean[] vst;
+	static int s, max = 0;
+	static void func(int cnt, int[] seq) {
+		if(cnt == s) {
+			Map<Character, Integer> map = new HashMap<>();
+			for(int i = 0; i < s; i++) map.put(carr[seq[i]], 9 - i);
+			int sum = 0;
+			for(String str : arr) {
+				int tmp = 0;
+				for(char c : str.toCharArray())tmp = (tmp * 10) + map.get(c);
+				sum += tmp;
+			}
+			max = Math.max(sum, max);
+			return;
+		}
+		for(int i = 0; i < s; i++) {
+			if(vst[i] == false) {
+				vst[i] = true;
+				seq[cnt] = i;
+				func(cnt + 1, seq);
+				vst[i] = false;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[26];
+		Arrays.fill(num, -1);
+		int n = sc.nextInt(), idx = 0;
+		arr = new String[n];
+		Set<Character> set = new HashSet<>();
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.next();
+			for(char c : arr[i].toCharArray()) {
+				if(!set.contains(c)) carr[idx++] = c;
+				set.add(c);
+			}
+		}
+		s = set.size();
+		vst = new boolean[s];
+		carr = Arrays.copyOfRange(carr, 0, s);
+		func(0, new int[s]);
+		
+		System.out.println(max);
+	}
+}
