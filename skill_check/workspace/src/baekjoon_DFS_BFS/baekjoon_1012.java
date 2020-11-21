6ff6f617b04da4d2f143e529f62f05eb1a4ac3d9
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-21 23:59:18 +0900

    bfs

--- skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_1012.java
+++ skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_1012.java
@@ -1,42 +1,37 @@
 package baekjoon_DFS_BFS;
 import java.util.*;
 public class baekjoon_1012 {
-	static int[] W = new int [] {1, 0, -1, 0};
-	static int[] H = new int [] {0, 1, 0, -1};
-	public static int[][] find(int[][] farm, int i, int j){
-		Queue<int[]> que = new LinkedList<>();
-		farm[i][j] = 0;
-		que.add(new int[] {i, j});
-		while(!que.isEmpty()) {
-			int[] tmp = que.poll();
-			for(int k=0; k<4; k++) {
-				int ii = tmp[0] + W[k];
-				int jj = tmp[1] + H[k];
-				if(0 <= ii && ii < farm.length && 0 <= jj && jj < farm[0].length) {
-					if(farm[ii][jj] == 1) {
-						que.add(new int [] {ii, jj});
-						farm[ii][jj] = 0;
-					}					
+	static int[][] wh = new int[][] {{1, 0, -1, 0}, {0, 1, 0, -1}};
+	public static void bfs(int[][] arr, int i, int j){
+		Queue<int[]> q = new LinkedList<>();
+		arr[i][j] = 0;
+		q.add(new int[] {i, j});
+		while(!q.isEmpty()) {
+			int[] p = q.poll();
+			for(int k = 0; k < 4; k++) {
+				int a = p[0] + wh[0][k];
+				int b = p[1] + wh[1][k];
+				if(0 <= a && a < arr.length && 0 <= b && b < arr[0].length) {
+					if(arr[a][b] == 1) {
+						q.add(new int [] {a, b});
+						arr[a][b] = 0;
+					}
 				}
 			}
 		}
-		return farm;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tc = sc.nextInt();
 		while(tc-->0) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			int k = sc.nextInt();
-			int cnt = 0;
-			int [][] farm = new int [n][m];
-			while(k-->0) farm[sc.nextInt()][sc.nextInt()] = 1;
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<m; j++) {
-					if(farm[i][j] == 1) {
+			int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt(), cnt = 0;
+			int[][] arr = new int[n][m];
+			for(int i = 0; i < k; i++) arr[sc.nextInt()][sc.nextInt()] = 1;
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					if(arr[i][j] == 1) {
 						cnt++;
-						farm = find(farm, i, j);
+						bfs(arr, i, j);
 					}
 				}
 			}
--- skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2210.java
+++ skill_check/workspace/src/baekjoon_DFS_BFS/baekjoon_2210.java
@@ -2,23 +2,29 @@ package baekjoon_DFS_BFS;
 
 import java.util.*;
 public class baekjoon_2210 {
-	static int[][] arr = new int[5][5], wh = {{0, 1, 0, -1}, {1, 0, -1, 0}};
-	static Set<Integer> set = new HashSet<>();
-	static void dfs(int x, int y, int cnt, int n) {
-		if(cnt == 6) set.add(n);
-		else {
-			for(int i = 0; i < 4; i++) {
-				int a = x + wh[0][i], b = y + wh[1][i];
-				if(0 <= a && 0 <= b && a < 5 && b < 5) {
-					dfs(a, b, cnt + 1, n * 10 + arr[a][b]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[5][5], wh = {{0, 1, 0, -1}, {1, 0, -1, 0}};
+		Set<Integer> set = new HashSet<>();
+		for(int i = 0; i < 25; i++) arr[i/5][i%5] = sc.nextInt();
+		for(int i = 0; i < 25; i++) {
+			Queue<int[]> q = new LinkedList<>();
+			q.add(new int[] {i / 5, i % 5, arr[i/5][i%5], 1});
+			while(!q.isEmpty()) {
+				int[] p = q.poll();
+				int x = p[0], y = p[1], v = p[2], c = p[3];
+				if(c == 6) {
+					set.add(v);
+					continue;
+				}
+				for(int j = 0; j < 4; j++) {
+					int a = x + wh[0][j], b = y + wh[1][j];
+					if(0 <= a && 0 <= b && a < 5 && b < 5) {
+						q.add(new int[] {a, b, v * 10 + arr[a][b], c + 1});
+					}
 				}
 			}
 		}
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		for(int i = 0; i < 25; i++) arr[i/5][i%5] = sc.nextInt();
-		for(int i = 0; i < 25; i++) dfs(i/5, i%5, 0, 0);
 		System.out.print(set.size());
 	}
 }
