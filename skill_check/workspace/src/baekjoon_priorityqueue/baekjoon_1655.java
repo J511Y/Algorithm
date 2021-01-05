b5f3e7cc92f8a455ff35f7c8a1032d6afe4de153
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-05 21:26:15 +0900

    백준_1655_가운데를 말해요_우선순위 큐

--- skill_check/workspace/src/baekjoon_priorityqueue/baekjoon_1655.java
+++ skill_check/workspace/src/baekjoon_priorityqueue/baekjoon_1655.java
@@ -2,13 +2,16 @@ package baekjoon_priorityqueue;
 // https://www.acmicpc.net/problem/1655
 // 가운데를 말해요
 import java.util.*;
+import java.io.*;
 public class baekjoon_1655 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int Int(String s) { return Integer.parseInt(s); }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PriorityQueue<Integer> min = new PriorityQueue<Integer>((a, b)->{return a - b;}), max = new PriorityQueue<Integer>((a, b)->{return b - a;});
-		int n = sc.nextInt();
+		int n = Int(br.readLine());
+        StringBuffer sb = new StringBuffer();
 		for(int i = 0; i < n; i++) {
-			int next = sc.nextInt();
+			int next = Int(br.readLine());
 			if(min.size() == max.size()) max.add(next);
 			else min.add(next);
 			
@@ -17,7 +20,8 @@ public class baekjoon_1655 {
 				min.add(max.poll());
 				max.add(a);
 			}
-			System.out.println(max.peek());
+			sb.append(max.peek() + "\n");
 		}
+        System.out.print(sb);
 	}
 }
