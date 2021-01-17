4ccba0206bcdb3c9bc5a861a5571a4bda1855b9a
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-17 17:09:23 +0900

    백준_1743_DFS_수류탄

---
+++ skill_check/workspace/baekjoon/baekjoon_DFS_BFS/baekjoon_1743.java
@@ -0,0 +1,31 @@
+package baekjoon_DFS_BFS;
+
+import java.util.*;
+public class baekjoon_1743 {
+	static int r, c, q, cnt, max = 0;
+	static int[][] map, wh = {{0, 1, 0, -1}, {1, 0, -1, 0}};
+	static void dfs(int x, int y) {
+		map[x][y] = 0;
+		cnt++;
+		for(int i = 0; i < 4; i++) {
+			int a = x + wh[0][i], b = y + wh[1][i];
+			if(0 <= a && 0 <= b && a < r && b < c && map[a][b] == 1) dfs(a, b);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		r = sc.nextInt(); c = sc.nextInt(); q = sc.nextInt();
+		map = new int[r][c];
+		for(int i = 0; i < q; i++) map[sc.nextInt()-1][sc.nextInt()-1] = 1;
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				if(map[i][j] == 1) {
+					cnt = 0;
+					dfs(i, j);
+					max = Math.max(max, cnt);
+				}
+			}
+		}
+		System.out.print(max);
+	}
+}
