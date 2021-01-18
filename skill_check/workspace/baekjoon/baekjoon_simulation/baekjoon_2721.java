b4d9a017e19ea946d9596f3295c9244e486b6b4e
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-18 23:20:00 +0900

    백준_2721_구현_삼각수

---
+++ skill_check/workspace/baekjoon/baekjoon_simulation/baekjoon_2721.java
@@ -0,0 +1,15 @@
+package baekjoon_simulation;
+
+import java.util.*;
+public class baekjoon_2721 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int q = sc.nextInt();
+		while(q-->0) {
+			int n = sc.nextInt();
+			long ans = 0;
+			for(int i = 1; i <= n; i++) ans += i * ((i + 1) * (i + 2) / 2);
+			System.out.println(ans);
+		}
+	}
+}
