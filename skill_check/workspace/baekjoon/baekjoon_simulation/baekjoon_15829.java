938943b6db4eb064a4b73f228752811355397366
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-18 23:09:26 +0900

    백준_15829_모듈러연산_Hashing

---
+++ skill_check/workspace/baekjoon/baekjoon_simulation/baekjoon_15829.java
@@ -0,0 +1,15 @@
+package baekjoon_simulation;
+
+import java.util.*;
+public class baekjoon_15829 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong(), rst = 0, r = 1, m = 1234567891;
+		byte[] arr = sc.next().getBytes();
+		for(int i = 0; i < n; i++) {
+			rst = (rst + (arr[i] - 'a' + 1) * r) % m;
+			r = (r * 31) % m;
+		}
+		System.out.print(rst);
+	}
+}
