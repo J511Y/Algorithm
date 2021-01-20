0e097fc820aea8d55c5d418a260bfd893d1868c2
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-20 21:26:59 +0900

    백준_9095_DP_1,2,3더하기

--- skill_check/workspace/baekjoon/baekjoon_DP/baekjoon_9095.java
+++ skill_check/workspace/baekjoon/baekjoon_DP/baekjoon_9095.java
@@ -1,20 +1,10 @@
 package baekjoon_DP;
 import java.util.*;
-public class baekjoon_9095 {
-	static int func(int ori, int num){
-        if (num == ori) return 1;
-        int sum = 0;
-        for (int i = 1; i < 4; i++){
-            if (num + i > ori) break;
-            sum += func(ori, num + i);
-        }
-        return sum;
-    }
+class baekjoon_9095 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int rnd = sc.nextInt();
-        while(rnd-->0){
-            System.out.println(func(sc.nextInt(), 0));
-        }
+        int[] a = {1,1,2,4,7,13,24,44,81,149,274};
+        int t = sc.nextInt();
+        while(t-->0) System.out.println(a[sc.nextInt()]);
     }
 }
