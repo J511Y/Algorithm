cd69c79b02e4bb12ed8b3e3ff6bfa070ed8cb980
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-24 08:27:55 +0900

    백준_9655_돌 게임_DP인줄알았으나 그냥 adhoc

---
+++ skill_check/workspace/baekjoon/baekjoon_DP/baekjoon_9655.java
@@ -0,0 +1,7 @@
+package baekjoon_DP;
+
+public class baekjoon_9655 {
+	public static void main(String[] args) {
+		System.out.print(new java.util.Scanner(System.in).nextInt() % 2 == 0 ? "CY" : "SK");
+	}
+}
