7a2b105c88bbcf52bdfae2b2df613ff072e8ef1a
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-26 22:37:42 +0900

    백준_2737_연속 합_수학

---
+++ skill_check/workspace/baekjoon/baekjoon_math/baekjoon_2737.java
@@ -0,0 +1,14 @@
+package baekjoon_math;
+
+import java.util.*;
+public class baekjoon_2737 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+			int n = sc.nextInt() - 1, cnt = 0;
+			for(int i = 2; (n -= i) >= 0; i++) cnt += n % i == 0 ? 1 : 0;
+			System.out.println(cnt);
+		}
+	}
+}
