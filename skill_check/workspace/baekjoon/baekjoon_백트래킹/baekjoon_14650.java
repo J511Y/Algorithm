82369e9bbc17c9c8ee5eef0236aa5ff7ab41e079
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-31 21:30:11 +0900

    백준_14650_걷다보니 신천역 삼 (Small)_DB

--- skill_check/workspace/baekjoon/baekjoon_백트래킹/baekjoon_14650.java
+++ skill_check/workspace/baekjoon/baekjoon_백트래킹/baekjoon_14650.java
@@ -1,18 +1,8 @@
 package baekjoon_백트래킹;
 
-public class baekjoon_14650 {
-	static int n, cnt = 0;
-	static void bt(int c, int v) {
-		if(c == n) cnt += v % 3 == 0 ? 1 : 0;
-		else {
-			if(v != 0) bt(c + 1, v * 10 + 0);
-			bt(c + 1, v * 10 + 1);
-			bt(c + 1, v * 10 + 2);
-		}
-	}
+class baekjoon_14650 {
 	public static void main(String[] args) {
-		n = new java.util.Scanner(System.in).nextInt();
-		bt(0, 0);
-		System.out.print(cnt);
+		int[] a = {0,2,6,18,54,162,486,1458,4374};
+		System.out.print(a[new java.util.Scanner(System.in).nextInt()-1]);
 	}
 }
