d1d8956c7fee5c7eac9226fa15a4460aa725ccf6
J511Y <47891005+J511Y@users.noreply.github.com>
2021-01-31 21:31:46 +0900

    백준_14650_걷다보니 신천역 삼(small)_숏코딩

--- skill_check/workspace/baekjoon/baekjoon_백트래킹/baekjoon_14650.java
+++ skill_check/workspace/baekjoon/baekjoon_백트래킹/baekjoon_14650.java
@@ -2,7 +2,6 @@ package baekjoon_백트래킹;
 
 class baekjoon_14650 {
 	public static void main(String[] args) {
-		int[] a = {0,2,6,18,54,162,486,1458,4374};
-		System.out.print(a[new java.util.Scanner(System.in).nextInt()-1]);
+		System.out.print((int)Math.pow(3, new java.util.Scanner(System.in).nextInt()-2) * 2);
 	}
 }
