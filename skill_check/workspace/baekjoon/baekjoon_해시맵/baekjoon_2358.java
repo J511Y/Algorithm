361dd8427ed55682a35017a8e3fbabe33c573a76
J511Y <47891005+J511Y@users.noreply.github.com>
2021-05-12 02:10:21 +0900

    백준_2358_평행선_해시맵

--- skill_check/workspace/baekjoon/baekjoon_해시맵/baekjoon_2358.java
+++ skill_check/workspace/baekjoon/baekjoon_해시맵/baekjoon_2358.java
@@ -4,15 +4,13 @@ import java.util.*;
 public class baekjoon_2358 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(), sum = 0;
+		int n = sc.nextInt();
 		Map<Integer, Integer> x = new HashMap<>(), y = new HashMap<>();
 		for(int i = 0; i < n; i++) {
 			int a = sc.nextInt(), b = sc.nextInt();
 			x.put(a, x.getOrDefault(a, 0) + 1);
 			y.put(b, y.getOrDefault(b, 0) + 1);
 		}
-		sum += (int)x.values().stream().filter(a -> a > 1).count();
-		sum += (int)y.values().stream().filter(a -> a > 1).count();
-		System.out.print(sum);
+		System.out.print(x.values().stream().filter(a -> a > 1).count() + y.values().stream().filter(a -> a > 1).count());
 	}
 }
