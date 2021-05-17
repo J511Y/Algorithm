b39684568bd060f6061e1789e3723cb1d7555165
J511Y <47891005+J511Y@users.noreply.github.com>
2021-05-18 01:02:39 +0900

    코드 길이 줄여봄

--- skill_check/workspace/baekjoon/baekjoon_문자열기본/baekjoon_10096.java
+++ skill_check/workspace/baekjoon/baekjoon_문자열기본/baekjoon_10096.java
@@ -2,6 +2,9 @@ package baekjoon_문자열기본;
 
 import java.util.*;
 public class baekjoon_10096 {
+	static void Print(char[] arr, int l, int r) {
+		System.out.print(new String(Arrays.copyOfRange(arr, l, r)));
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -12,10 +15,10 @@ public class baekjoon_10096 {
 			for(int i = 1; i < n; i++) allsame &= arr[i] == arr[i-1];
 			for(int i = 0, l = 0, r = n / 2; i < n / 2; i++, l++, r++) left &= arr[l] == arr[r];
 			for(int i = 0, l = 1, r = n / 2 + 1; i < n / 2; i++, l++, r++) right &= arr[l] == arr[r];
-			if(allsame) System.out.print(new String(Arrays.copyOfRange(arr, 0, n / 2)));
+			if(allsame) Print(arr, 0, n / 2);
 			else if(left && right) System.out.print("NOT UNIQUE");
-			else if(left) System.out.print(new String(Arrays.copyOfRange(arr, 0, n / 2)));
-			else if(right) System.out.print(new String(Arrays.copyOfRange(arr, n / 2 + 1, n)));
+			else if(left) Print(arr, 0, n / 2);
+			else if(right) Print(arr, n / 2 + 1, n);
 			else {
 				boolean flag = false;
 				for(int i = 0, l = 0, r = n / 2; i < n / 2; i++, l++, r++) {
@@ -30,7 +33,7 @@ public class baekjoon_10096 {
 						}
 					}
 				}
-				if(flag) System.out.print(new String(Arrays.copyOfRange(arr, 0, n / 2)));
+				if(flag) Print(arr, 0, n / 2);
 				else {
 					for(int i = 0, l = 0, r = n / 2 + 1; i < n / 2; i++, l++, r++) {
 						if(arr[l] != arr[r]) {
@@ -44,7 +47,7 @@ public class baekjoon_10096 {
 							}
 						}
 					}
-					System.out.print(new String(Arrays.copyOfRange(arr, n / 2 + 1, n)));
+					Print(arr, n / 2 + 1, n);
 				}
 			}
 		}
