59b2be160af4e6cf2eb00e8aa5628aab79357cfd
J511Y <47891005+J511Y@users.noreply.github.com>
2021-05-20 01:29:14 +0900

    백준_13909_창문 닫기_기본수학

---
+++ skill_check/workspace/baekjoon/baekjoon_기본수학/baekjoon_13909.java
@@ -0,0 +1,7 @@
+package baekjoon_기본수학;
+
+public class baekjoon_13909 {
+	public static void main(String[] args) {
+		System.out.print((int)Math.sqrt(new java.util.Scanner(System.in).nextInt()));
+	}
+}
