c0f8f24b95f376ec40287bca7b1705bec013031b
J511Y <47891005+J511Y@users.noreply.github.com>
2021-05-20 02:59:29 +0900

    백준_3344_nqueen_애드혹

---
+++ skill_check/workspace/baekjoon/baekjoon_애드혹/baekjoon_3344.java
@@ -0,0 +1,23 @@
+package baekjoon_æ÷µÂ»§;
+
+import java.util.*;
+public class baekjoon_3344 {
+	public static void main(String[] args) {
+		int n = new Scanner(System.in).nextInt();
+		Deque<Integer> even = new LinkedList<>(), odd = new LinkedList<>();
+		for(int i = 1; i <= n; i++) (i % 2 == 0 ? even : odd).addLast(i);
+		if(n % 6 == 2) {
+			int a = odd.pollFirst(), b = odd.pollFirst(), c = odd.pollFirst();
+			odd.addLast(c);
+			odd.addFirst(a);
+			odd.addFirst(b);
+		}
+		if(n % 6 == 3) {
+			even.addLast(even.pollFirst());
+			odd.addLast(odd.pollFirst());
+			odd.addLast(odd.pollFirst());
+		}
+		while(!even.isEmpty()) System.out.println(even.pollFirst());
+		while(!odd.isEmpty()) System.out.println(odd.pollFirst());
+	}
+}
