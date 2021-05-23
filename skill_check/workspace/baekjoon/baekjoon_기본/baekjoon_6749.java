31f5c87e9644f1c0c973dc00ff20ee59db5a0904
J511Y <47891005+J511Y@users.noreply.github.com>
2021-05-23 21:56:58 +0900

    백준_6749_기본

---
+++ skill_check/workspace/baekjoon/baekjoon_기본/baekjoon_6749.java
@@ -0,0 +1,10 @@
+package baekjoon_±âº»;
+
+import java.util.*;
+public class baekjoon_6749 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt();
+		System.out.print(b + (b - a));
+	}
+}
