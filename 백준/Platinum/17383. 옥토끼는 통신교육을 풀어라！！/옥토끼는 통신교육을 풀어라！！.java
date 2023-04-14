6ed20495e8a683b3dc19e00f0fe81c7c1478533b
유준혁 <47891005+J511Y@users.noreply.github.com>
2023-04-14 22:08:32 +0900

    [Platinum IV] Title: 옥토끼는 통신교육을 풀어라!!, Time: 908 ms, Memory: 99760 KB -BaekjoonHub

---
+++ 백준/Platinum/17383. 옥토끼는 통신교육을 풀어라！！/옥토끼는 통신교육을 풀어라！！.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] arr = new long[n];
+		long l = 1, r = 0;
+		for(int i = 0; i < n; i++) r = Math.max(r, arr[i] = sc.nextInt());
+		while(l < r) {
+			long mid = (l + r) / 2, sum = 0;
+			for(long i : arr) sum += 2 - ((i / mid) + (i % mid > 0 ? 1 : 0));
+			if(sum > 0) r = mid;
+			else l = mid + 1;
+		}
+		System.out.print(r);
+	}
+}
